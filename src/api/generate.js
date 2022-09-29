9ab68eeb71c92333ade0c2b5408d2d4ebf8f844a
digitalmarketing@alltech.com <digitalmarketing@alltech.com>
2022-09-29 16:03:25 -0400

    aibot

---
+++ src/api/generate.js
@@ -0,0 +1,22 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: generatePrompt(req.body.animal),
+    max_tokens: 100,
+    temperature: 0.6,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `${capitalizedAnimal}`;
+}
